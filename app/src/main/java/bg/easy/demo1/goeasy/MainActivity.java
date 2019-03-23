53785d5a36322dd71acf74e291c390f85edfe802
Velis <velislava_n@abv.bg>
2019-03-23 22:28:09 +0200

    Hiding the previous action bar on main activity load.

--- app/src/main/java/bg/easy/demo1/goeasy/MainActivity.java
+++ app/src/main/java/bg/easy/demo1/goeasy/MainActivity.java
@@ -1,11 +1,10 @@
 package bg.easy.demo1.goeasy;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -14,7 +13,7 @@ import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    static ActionBar ab;
     private static String WV_URL = MyBrowser.path;
     WebView wv1;
     private SwipeRefreshLayout swipe;
@@ -24,9 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ab = getSupportActionBar();
         wv1 = findViewById(R.id.webView);
         swipe = findViewById(R.id.swiperefresh);
-
+        getSupportActionBar().setTitle(R.string.bar_title);
         //Webview settings; defaults are customized for best performance
         WebSettings webSettings = wv1.getSettings();
         webSettings.setSupportZoom(true);
@@ -34,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true);
         webSettings.setJavaScriptEnabled(true);
 
-        wv1.setWebViewClient(new MyBrowser());
+        wv1.setWebViewClient(new MyBrowser()
+
+        );
         loadPage(WV_URL);
 
         /*
@@ -54,13 +56,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadPage(String url) {
-        final String url2 = url;
         if (DetectConnection.isInternetAvailable(this)) {
             wv1.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
-            wv1.loadUrl(url2);
+            wv1.loadUrl(url);
         } else {
             wv1.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-            wv1.loadUrl(url2);
+            wv1.loadUrl(url);
             AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
             alertDialog.setTitle("Error");
             alertDialog.setMessage("Check your internet connection and try again.");
--- app/src/main/java/bg/easy/demo1/goeasy/MyBrowser.java
+++ app/src/main/java/bg/easy/demo1/goeasy/MyBrowser.java
@@ -1,16 +1,31 @@
 package bg.easy.demo1.goeasy;
 
 import android.net.Uri;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 class MyBrowser extends WebViewClient {
-    static String path= Uri.parse("https://demo1.easy.bg").toString();
+    static String path = Uri.parse("https://demo1.easy.bg").toString();
 
-     //Loads every clicked link inside my webVew
+    //Loads every clicked link inside my webVew
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
         view.loadUrl(url);
         return true;
     }
+
+    @Override
+    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+        super.onReceivedError(view, request, error);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+
+        super.onPageFinished(view, url);
+        MainActivity.ab.hide();  //Hide the previous action bar
+    }
 }
+
