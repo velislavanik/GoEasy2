f5c462f8287927b41f5c0466550f3a4be9f15407
Velis <velislava_n@abv.bg>
2019-03-23 16:34:58 +0200

    Adding DetectConnection class and checking on onCreate activity+ adding Load method with paramether - url

--- app/src/main/java/bg/easy/demo1/goeasy/MainActivity.java
+++ app/src/main/java/bg/easy/demo1/goeasy/MainActivity.java
@@ -1,20 +1,63 @@
 package bg.easy.demo1.goeasy;
 
+import android.content.Context;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-    WebView wv1;
 
+
+    private static String WV_URL      = MyBrowser.path;
+    private boolean conn;
+    WebView wv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // conn=DetectConnection.isInternetAvailable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wv1=findViewById(R.id.webView);
+        wv1 = findViewById(R.id.webView);
+        //Webview settings; defaults are customized for best performance
+        WebSettings webSettings = wv1.getSettings();
+        webSettings.setSupportZoom(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+
         wv1.setWebViewClient(new MyBrowser());
-        wv1.loadUrl(MyBrowser.path);
+        loadPage(WV_URL);
+
+    }
+        public void loadPage(String url){
+            if (DetectConnection.isInternetAvailable(this)) {
+                wv1.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+            } else {
+                wv1.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+            }
+           wv1.loadUrl(url);
+            // TO DO Syobshti na usera, 4e nmqa net
+  }
+
+    //Action on back key tap/click
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (wv1.canGoBack()) {
+                        wv1.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
